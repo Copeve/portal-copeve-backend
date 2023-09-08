f43c653729c5ac64d50ec045a103fd799ba93392
André Oliveira <andrev-eng@ufmg.br>
2023-09-08 12:54:34 -0300

    Implementação do Fuzzy

---
+++ config/plugins.ts
@@ -0,0 +1,53 @@
+module.exports = ({ env }) => ({
+    // ...
+
+    "fuzzy-search": {
+        enabled: true,
+        config: {
+            contentTypes: [
+                {
+                    uid: "api::noticia.noticia",
+                    modelName: "noticia",
+                    transliterate: true,
+                    fuzzysortOptions: {
+                        characterLimit: 300,
+                        threshold: -600,
+                        limit: 10,
+                        keys: [
+                            {
+                                name: "titulo",
+                                weight: 100,
+                            },
+                            {
+                                name: "subtitulo",
+                                weight: 0,
+                            },
+                            {
+                                name: "noticia",
+                                weight: -100,
+                            },
+                        ],
+                    }
+                },
+                {
+                    uid: "api::concurso.concurso",
+                    modelName: "concurso",
+                    transliterate: true,
+                    fuzzysortOptions: {
+                        characterLimit: 300,
+                        threshold: -600,
+                        limit: 10,
+                        keys: [
+                            {
+                                name: "nome",
+                                weight: 100,
+                            }
+                        ],
+                    }
+                }
+            ],
+        },
+    },
+
+    // ...
+});

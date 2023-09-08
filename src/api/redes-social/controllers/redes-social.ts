6a86bde394fa6c2b594db1e5b1540c48b301e014
André Oliveira <andrev-eng@ufmg.br>
2023-09-08 11:34:58 -0300

    Updage, rodape e redes sociais.

---
+++ src/api/redes-social/controllers/redes-social.ts
@@ -0,0 +1,7 @@
+/**
+ * redes-social controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::redes-social.redes-social');
---
+++ src/api/redes-social/routes/redes-social.ts
@@ -0,0 +1,7 @@
+/**
+ * redes-social router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::redes-social.redes-social');

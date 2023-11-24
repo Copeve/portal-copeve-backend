cacff1a4c0a6518a95ea866d50e7a4ff8e923e09
André Oliveira <andrev-eng@ufmg.br>
2023-11-23 21:00:10 -0300

    Inclusão trabalhe conosco

---
+++ src/api/trabalhe-conosco/controllers/trabalhe-conosco.ts
@@ -0,0 +1,7 @@
+/**
+ * trabalhe-conosco controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::trabalhe-conosco.trabalhe-conosco');
---
+++ src/api/trabalhe-conosco/services/trabalhe-conosco.ts
@@ -0,0 +1,7 @@
+/**
+ * trabalhe-conosco service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::trabalhe-conosco.trabalhe-conosco');
